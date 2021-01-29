dfe64c833c15e7839968571e13c6c19b91e2f4c1
kurwitz3 <kurwitz3@aol.com>
2021-01-29 18:56:30 -0500

    commit tree b4746a6ffc43ac33ad72eedd7ed1d979b1394f63

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name,email){
+    let forData = {
+        name: name ,
+        email: email  
+    };
+    let configObj = {
+        method: 'POST',
+        headers:{    
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(forData)
+    };
+  return fetch('http://localhost:3000/users',configObj)
+  .then(function(response){
+   return response.json();
+  })
+  .then(function(object){
+      let h2 = document.createElement('h2')
+      h2.innerHTML = object.id
+      document.body.appendChild(h2)
+    })
+  .catch(function(error) {
+    alert("Unauthorized Access");
+    let h3 = document.createElement('h3')
+    h3.innerHTML = error.message
+    document.body.appendChild(h3)
+  }); 
+
+}
